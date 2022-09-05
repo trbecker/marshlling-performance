9dbfdd4b6e258ebaaaa7509e785a567e4282df1f
Thiago Rafael Becker <thiago.becker@gmail.com>
2022-09-05 18:02:19 -0300

    plot-eps: remove the dots for clearer display

--- asn.1/asn1c-c/plot-eps.sh
+++ asn.1/asn1c-c/plot-eps.sh
@@ -6,8 +6,8 @@ set terminal png size 1000,300 enhanced font "Helvetica,20"
 set term eps
 set output 'asn1_int_array_$1_elements.eps'
 set ylabel 'time (ms)'
-plot '$1/results.dat' using 1 with linespoints title 'encoding', \
-     '$1/results.dat' using 2 with linespoints title 'decoding'
+plot '$1/results.dat' using 1 with lines title 'encoding', \
+     '$1/results.dat' using 2 with lines title 'decoding'
 EOF
 }
 
